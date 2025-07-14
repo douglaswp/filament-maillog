a755f0e02b5ab4e33c2842d04e965d30142d09ee
Douglas WP <douglaswp@users.noreply.github.com>
2025-07-14 17:23:53 -0300

    Add Portuguese (Brazil) translation

---
+++ resources/lang/pt_BR/filament-maillog.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'navigation.group' => 'Configurações',
+
+    'navigation.maillog.label' => 'Log de E-mails',
+    'navigation.maillog.plural-label' => 'Logs de E-mails',
+
+    'table.heading' => 'Mail Logs',
+
+    'column.status' => 'Status',
+    'column.subject' => 'Assunto',
+    'column.to' => 'Para',
+    'column.from' => 'De',
+    'column.cc' => 'CC',
+    'column.bcc' => 'BCC',
+    'column.message_id' => 'Message ID',
+    'column.delivered' => 'Entregue',
+    'column.opened' => 'Aberto',
+    'column.bounced' => 'Rejeitado',
+    'column.complaint' => 'Reclamação',
+    'column.body' => 'Body',
+    'column.headers' => 'Headers',
+    'column.attachments' => 'Anexos',
+    'column.data' => 'Data',
+    'column.created_at' => 'Criado em',
+    'column.updated_at' => 'Atualizado em',
+
+    'filters.status' => 'Status',
+    'filters.created_from' => 'Criado a partir de',
+    'filters.created_until' => 'Criado até',
+];
